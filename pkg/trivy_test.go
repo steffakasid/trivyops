80d0fb38f3cf043c0ae10621bdb45fb1f7a26a56
steffakasid <github@steffen-rumpf.de>
2022-03-17 21:11:27 +0100

    test: Add some more unittests

--- pkg/trivy_test.go
+++ pkg/trivy_test.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"testing"
 
+	dbtypes "github.com/aquasecurity/trivy-db/pkg/types"
+	"github.com/aquasecurity/trivy/pkg/types"
 	"github.com/steffakasid/trivy-scanner/pkg/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/xanzy/go-gitlab"
@@ -79,4 +81,72 @@ func TestGetTrivyIgnore(t *testing.T) {
 		assert.Len(t, result.Ignore, 0)
 		assert.Nil(t, err)
 	})
+}
+
+func TestCheck(t *testing.T) {
+	trivyResults := &TrivyResults{
+		&trivy{
+			ProjId: 1,
+			ReportResult: types.Results{
+				types.Result{
+					Vulnerabilities: []types.DetectedVulnerability{
+						{
+							Vulnerability: dbtypes.Vulnerability{Severity: "CRITICAL"},
+						},
+						{
+							Vulnerability: dbtypes.Vulnerability{Severity: "HIGH"},
+						},
+					},
+				},
+			},
+		},
+		&trivy{
+			ProjId: 2,
+			ReportResult: types.Results{
+				types.Result{
+					Vulnerabilities: []types.DetectedVulnerability{
+						{
+							Vulnerability: dbtypes.Vulnerability{Severity: "LOW"},
+						},
+						{
+							Vulnerability: dbtypes.Vulnerability{Severity: "HIGH"},
+						},
+					},
+				},
+			},
+		},
+	}
+	trivyResults.Check()
+	assert.Equal(t, 2, (*trivyResults)[0].Vulnerabilities.Count)
+	assert.Equal(t, 1, (*trivyResults)[0].Vulnerabilities.Critical)
+	assert.Equal(t, 1, (*trivyResults)[0].Vulnerabilities.High)
+	assert.Equal(t, 2, (*trivyResults)[1].Vulnerabilities.Count)
+	assert.Equal(t, 0, (*trivyResults)[1].Vulnerabilities.Critical)
+	assert.Equal(t, 1, (*trivyResults)[1].Vulnerabilities.High)
+}
+
+func TestSummary(t *testing.T) {
+	vullies := []types.DetectedVulnerability{
+		{
+			Vulnerability: dbtypes.Vulnerability{Severity: "HIGH"},
+		},
+		{
+			Vulnerability: dbtypes.Vulnerability{Severity: "LOW"},
+		},
+		{
+			Vulnerability: dbtypes.Vulnerability{Severity: "CRITICAL"},
+		},
+		{
+			Vulnerability: dbtypes.Vulnerability{Severity: "OTHER"},
+		},
+		{
+			Vulnerability: dbtypes.Vulnerability{Severity: "HIGH"},
+		},
+	}
+	crit, high, med, low, other := GetSummary(vullies)
+	assert.Equal(t, 1, crit)
+	assert.Equal(t, 2, high)
+	assert.Equal(t, 0, med)
+	assert.Equal(t, 1, low)
+	assert.Equal(t, 1, other)
 }
